0e6fdcef66a9b7c554dffc4dbcef6a91d353a0ec
ganaa037 <tuvaangantumur@gmail.com>
2025-02-24 12:50:01 +0800

    screen

--- weathers/src/app/page.js
+++ weathers/src/app/page.js
@@ -1,5 +1,13 @@
+import { Circles } from "@/components/Index";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="grid grid-cols-2 h-screen w-[100%]">
+      <div className="bg-white w-[50%]  "></div>
+
+      <div className="bg-black w-[50%]"></div>
+      <Circles></Circles>
+    </div>
+  );
 }
---
+++ weathers/src/components/Radius.jsx
@@ -0,0 +1,7 @@
+export const Circles = () => {
+  return (
+    <div>
+      <div className="size-940px border-[940px] rounded-[940px]  border-black"></div>
+    </div>
+  );
+};
