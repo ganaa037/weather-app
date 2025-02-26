32ce67468d93a39a9c8434b2b0c90d82a6ea5e55
ganaa037 <tuvaangantumur@gmail.com>
2025-02-26 11:39:09 +0800

    init

--- weathers/src/app/page.js
+++ weathers/src/app/page.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen relative justify-center">
-      <div className="bg-white w-[50%]"></div>
-      <div className="bg-black w-[50%] "></div>
+    <div className="flex h-screen relative justify-center ">
+      <div className="bg-[#F3F4F6] w-[50%] h-screen"></div>
+      <div className="bg-black w-[50%] h-screen"></div>
       <div className="flex">
         <Circles className={"size-[1340px]"} />
         <Circles className={"size-[940px]"} />
@@ -18,7 +18,11 @@ export default function Home() {
       <Icon
         className={"/Users/24LP9559/Desktop/weather/weathers/public/group.svg "}
       ></Icon>
-      <Krakow></Krakow>
+      <Krakow
+        img={
+          "https://s3-alpha-sig.figma.com/img/3c6b/babb/0657324bf17d1bd5169b60a7fbcb80b1?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uVwKHLA2r0mB54mUCGA3sxRal85xaGrB10-WtvmM-SlHJ9avtIeSEY5L9pGLJd9sN9K2nlTm-t75gLnBzjZ5o630U8nXaaPzxY-Q~6W6xqde3xBb5l5DqGklTN241rg1PmtRLRUlH-AwkqcTz7DoMXnipYL~12HTmUZqiU8biX-Iv1E~5OpXDTeJuT2VKLSuAnDYSph060VwNsEtsapKYZBMvI5Fpfdzf1tzwiF~LwqfhzNFqozotdHJP20gxpwsKvMbw-CWA26NDtbr5j7uPjepLCTWwmZ-NvwDoZOLeARlpd5LtLfSabJOl7OUWV9~fm1X1PiJGr8zf95gezSSTw__"
+        }
+      ></Krakow>
     </div>
   );
 }
